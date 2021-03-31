552c3976c3ed2464343c1629a5f231cff6f1b9d7
Tom Pretty <tompretty2@gmail.com>
2021-03-31 12:03:30 +0100

    wip

--- support-workers/src/main/scala/com/gu/support/workers/lambdas/PaymentMethodExtensions.scala
+++ support-workers/src/main/scala/com/gu/support/workers/lambdas/PaymentMethodExtensions.scala
@@ -7,6 +7,7 @@ import com.gu.support.workers.{
   PayPalReferenceTransaction,
   PaymentMethod
 }
+import com.gu.support.workers.SepaPaymentMethod
 
 object PaymentMethodExtensions {
   implicit class PaymentMethodExtension[T <: PaymentMethod](self: T) {
@@ -14,6 +15,7 @@ object PaymentMethodExtensions {
       case _: CreditCardReferenceTransaction => "Stripe"
       case _: PayPalReferenceTransaction => "PayPal"
       case _: DirectDebitPaymentMethod | _: ClonedDirectDebitPaymentMethod => "DirectDebit"
+      case _: SepaPaymentMethod => "Sepa" // TODO FIX?
     }
   }
 }
