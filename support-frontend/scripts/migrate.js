c54e6739c39c0ad3d8807632ce6e5a9ea3261fd9
George Haberis <george.haberis@guardian.co.uk>
2021-04-23 16:15:38 +0100

    tweak script

--- support-frontend/scripts/migrate.js
+++ support-frontend/scripts/migrate.js
@@ -3,22 +3,33 @@ const { exec } = require('child_process');
 
 // copy assets to new folder called typescript and run this script
 
-glob('../typescript/**/*.{js,jsx}', (err, files) => {
+glob('../assets/helpers/abTests/**/*.{js,jsx}', (err, files) => {
   if (err) {
     console.log(err.message);
   } else {
-    files.reduce((accumulatorPromise, filePath) => accumulatorPromise.then(() =>
-      new Promise((resolve) => {
-        exec(`flow-to-ts --write --delete-source ${filePath} `, (error) => {
-          if (error) {
-            console.log(`Failed to migrate ${filePath}: ${error.message}`);
-            return;
-          }
+    const failures = [];
 
-          console.log(`Successfully migrated ${filePath}`);
+    const result = files.reduce((accumulatorPromise, filePath) => accumulatorPromise.then(() =>
+      new Promise((resolve) => {
+        exec(`flow-to-ts --write ${filePath} `, (error) => {
+          if (error) {
+            failures.push(`Failed to migrate ${filePath}: ${error.message}`);
+          } else {
+            console.log(`Successfully migrated ${filePath}`);
+          }
 
           resolve();
         });
       })), Promise.resolve());
+
+    result.then(() => {
+      if (failures.length) {
+        failures.forEach((failure) => {
+          console.log(failure);
+        });
+      } else {
+        console.log('All files successfully migrated!');
+      }
+    });
   }
 });
